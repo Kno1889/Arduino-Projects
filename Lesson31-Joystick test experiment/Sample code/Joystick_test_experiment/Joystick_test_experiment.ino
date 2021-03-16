91ebdc53654092feaf8b71d128de3220d0e69123
Kno1889 <56836650+Kno1889@users.noreply.github.com>
2021-03-16 00:41:47 -0400

    Add files via upload

---
+++ Lesson31-Joystick test experiment/Sample code/Joystick_test_experiment/Joystick_test_experiment.ino
@@ -0,0 +1,57 @@
+/*
+ * Author: SMRAZA KEEN
+ * Date:2016/6/29
+ * IDE V1.6.9
+ * Email:TechnicSmraza@outlook.com
+ * Function:
+ */
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // initializing LCD Digital Pins
+int xpotPin = A0; // initializing Joystick analog pins
+int ypotPin = A1;            
+int bpotPin = 6; 
+
+int xval=0;       //Initializing variables to hold x, y values of joystick output
+int yval=0;
+int bval=0;
+
+void setup()                                                                                     
+{
+  lcd.begin(16,2);   //Display Address
+  pinMode(xpotPin,INPUT); // defining input from joystick to Arduino board
+  pinMode(ypotPin,INPUT);
+  pinMode(bpotPin,INPUT);
+   
+  lcd.print("  Welcome to ");
+  lcd.setCursor(0,1);
+  lcd.print("     Smraza");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop()
+{ 
+  xval = analogRead(xpotPin);   //Read Values from the xpotPin
+  yval = analogRead(ypotPin);   
+  bval = digitalRead(bpotPin);   
+
+  lcd.clear();                 //clear display
+  lcd.setCursor(0, 0) ;       //Display position
+  lcd.print("X=");            //Display "X="
+  lcd.print(xval);
+  lcd.setCursor(7, 0) ;  
+  lcd.print("Y=");       
+  lcd.print(yval);  
+  
+  if (bval==LOW)
+  {
+    lcd.setCursor(0, 1) ;   
+    lcd.print("Button ON"); 
+  }
+  else
+  {
+  lcd.setCursor(0, 1) ;
+  lcd.print("Button OFF"); 
+  }
+  delay(150); //After 150ms the screen will be refreshed
+}
